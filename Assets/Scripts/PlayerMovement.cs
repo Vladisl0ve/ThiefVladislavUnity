d6778e3a584d29e874a493cd16538853b86a0b4b
Vladislove <vladislav.lapko@gmail.com>
2022-08-25 21:56:12 +0200

    Added animations for player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,49 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
 
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        //Grab references from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horInput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+        //Player flipping while moving
+        if (horInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //Set animator parameters
+        anim.SetBool("running", horInput != 0);
+        anim.SetBool("grounded", grounded);
+
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            body.velocity = new Vector2(body.velocity.x, speed);
+            grounded = true;
         }
     }
 }
